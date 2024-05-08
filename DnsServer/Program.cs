c71b5ecac71521929aac73eda663a9c83d51e163
MateuszKrawczyk <mateuszkrawczyk1996@gmail.com>
2024-05-08 20:27:21 +0200

    demo

--- DnsServer/Program.cs
+++ DnsServer/Program.cs
@@ -6,20 +6,25 @@ using DnsServer_Core.Message;
 
 Console.WriteLine("Hello, World!");
 
-var server = new DnsServer(IPEndPoint.Parse("127.0.0.2:53"), q =>
+var server = new DnsServer(IPEndPoint.Parse("194.182.72.177:53"), q =>
 {
     Console.WriteLine(q.Type + " " + (String.Join(".", q.Name)));
-    if (String.Join(".", q.Name) == "test.pl")
+    if (String.Join(".", q.Name).ToLower() == "dnstest.green-code.studio")
     {
         if (q.Type == QType.A)
             return new Answer() { Data = new byte[] { 1, 2, 3, 4 } };
         else if(q.Type == QType.TXT)
             return new Answer() { Data = new List<string> { "Hello", "World" }};
         else if(q.Type == QType.NS)
-            return new Answer() { Data = new List<string> { "test2", "pl" }};
-    }
-    else if (String.Join(".", q.Name) == "test.com")
+            return new Answer() { Data = new List<string> { "dnstest", "green-code", "studio"}};
+    }else if (String.Join(".", q.Name).ToLower() == "abcd.dnstest.green-code.studio")
     {
+        if (q.Type == QType.A)
+            return new Answer() { Data = new byte[] {99,99,99,99 } };
+    }else if (String.Join(".", q.Name).ToLower() == "google.dnstest.green-code.studio")
+    {
+        if (q.Type == QType.A)
+            return new Answer() { Data = new byte[] {216,58,209,46 } };
     }
 
     return null;
